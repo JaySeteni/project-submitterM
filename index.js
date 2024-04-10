dbcebe675578f0ca08aef47407ba2636869d8741
JaySeteni <jay.seteni@gmail.com>
2024-04-10 23:39:32 +0200

    messageSubmitter Last Commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const transmitter = document.getElementById("transmitter");
 const sendButton = document.getElementById("sendButton");
 const sendArea = document.getElementById("sendArea");
 
-
 sendButton.addEventListener('click', function(){
     const currentDate = new Date();
     const currentTime = currentDate.toLocaleTimeString();
@@ -13,13 +12,35 @@ sendButton.addEventListener('click', function(){
 
     console.log(message);
     if(message){
-        message =  transmitter.value.trim() + " " + " " + currentTime + " - " + currentDateFormatted;
-            const newLocal = contentTransmit.innerHTML = `<div class="contentNbutton"> <p class="contentNdelete">${message}</p> <button id="deleteButton" onclick="deleteMessage(this)">Delete</button></div>` + contentTransmit.innerHTML;
-            transmitter.value = "";
+        message =  transmitter.value.trim() + " " + " (" + currentTime + " - " + currentDateFormatted + ")";
+           contentTransmit.innerHTML = `<div class="contentNbutton"> <p class="contentNdelete">${message}</p> <button id="deleteButton" onclick="deleteMessage(this)">Delete</button></div>` + contentTransmit.innerHTML;
+           transmitter.value = "";
         }
     });
+
     function deleteMessage(button) {
     button.parentNode.remove();
 }
-// main project javascripting
+
+
+// const myForm = document.getElementById('myForm');
+// const inputName = myForm['name'].value;
+// const fNameInput = document.getElementById('fName');
+// const lNameInput = document.getElementById('lName');
+// const emailReg = document.getElementById('emailReg');
+// const phoneNo = document.getElementById('phoneNo');
+// const helpField = document.getElementById('helpField');
+// const submitBtn = document.getElementById('submitBtn');
+// const pwdReg = document.getElementById('pwdReg')
+
+
+// const storage = (,userNameInput,emailReg,pwdReg) =>{
+// fNameInput.innertext = "firstName: " + fNameInput.value;
+// lNameInput.innertext = "lastName: " + lNameInput.value;
+// emailReg.innertext = "Email: " + emailReg.value;
+// pwdReg.innertext = "password: " + pwdReg.value;
+
+// }
+
+// localStorage.setItem("stored", JSON.stringify('storage'));
 
